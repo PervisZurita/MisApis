ec0376f2cd83843954c6fa7ae51209796e89e2c7
perviszurita <pervis.zurita.valencia@utelvt.edu.ec>
2025-09-15 21:23:31 -0500

    modificando

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,10 @@
-import ejemplo from './ejemplo.routes.js';
+
 import pc from './pc.routes.js';
 import celular from './celular.routes.js'; 
 import { Router } from 'express';
 
 const indexRoutes = Router();
 
-indexRoutes.use('/ejemplo', ejemplo);
 indexRoutes.use('/pc', pc);
 indexRoutes.use('/celular', celular);
 
